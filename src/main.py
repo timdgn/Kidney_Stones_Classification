8334864613a55f27cc083e122699164c8a09ea39
timolp <timmothy86264020@gmail.com>
2023-05-05 00:06:53 +0100

    [Refactor] Docstring and comments

--- src/main.py
+++ src/main.py
@@ -7,6 +7,27 @@ from eda_reports import *
 
 
 def plot_roc_auc(fpr, tpr, auc_score):
+    """
+    Plots the ROC curve and the AUC score.
+
+    Parameters
+    ----------
+    fpr : array-like
+        The false positive rate values for different thresholds.
+    tpr : array-like
+        The true positive rate values for different thresholds.
+    auc_score : float
+        The area under the ROC curve.
+
+    Returns
+    -------
+    None
+
+    Outputs
+    -------
+    An image file named "ROC AUC Curve.png" in the docs folder.
+    A matplotlib figure showing the ROC curve and the AUC score.
+    """
 
     # Set the style of the plot
     sns.set_style("darkgrid")
@@ -34,6 +55,26 @@ def plot_roc_auc(fpr, tpr, auc_score):
 
 
 def plot_confusion_matrix(y_test, y_pred):
+    """
+    Plots the confusion matrix.
+
+    Parameters
+    ----------
+    y_test : array-like
+        The true labels of the test set.
+    y_pred : array-like
+        The predicted labels of the test set.
+
+    Returns
+    -------
+    None
+
+    Outputs
+    -------
+    An image file named "Confusion Matrix.png" in the docs folder.
+    A matplotlib figure showing the confusion matrix.
+    """
+
     # Plotting the confusion matrix
     cm = confusion_matrix(y_test, y_pred)
 
@@ -53,16 +94,54 @@ def plot_confusion_matrix(y_test, y_pred):
 
 
 def xgb_predict(X_train, y_train, X_test):
+    """
+    Trains and predicts a binary classifier using XGBoost.
 
+    Parameters
+    ----------
+    X_train : array-like
+        The features of the training set.
+    y_train : array-like
+        The labels of the training set.
+    X_test : array-like
+        The features of the test set.
+
+    Returns
+    -------
+    y_pred : array-like
+        The predicted labels of the test set.
+    """
+
+    # Create the model and fit it
     model = XGBClassifier()
     model.fit(X_train, y_train)
 
+    # Predict with the xgb model
     y_pred = model.predict(X_test)
 
     return y_pred
 
 
 def evaluate_model(y_test, y_pred):
+    """
+    Evaluates the performance of the binary classifier using Accuracy, F1 and AUC scores.
+
+    Parameters
+    ----------
+    y_test : array-like
+        The true labels of the test set.
+    y_pred : array-like
+        The predicted labels of the test set.
+
+    Returns
+    -------
+    None
+
+    Outputs
+    -------
+    Prints the accuracy, F1 score and AUC score of the classifier.
+    Calls the plot_confusion_matrix and plot_roc_auc functions to generate plots.
+    """
 
     # Calculate and print the accuracy score
     accuracy = accuracy_score(y_test, y_pred)
@@ -96,8 +175,10 @@ if __name__ == "__main__":
     y = train_df["target"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
 
+    # Predict with the xgb model
     y_pred = xgb_predict(X_train, y_train, X_test)
 
+    # Evaluate the results
     evaluate_model(y_test, y_pred)
 
     print("Finished ✅")
