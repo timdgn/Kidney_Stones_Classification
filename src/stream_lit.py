72f4df507ad4c2f1b3ac7233bcd10a8c13629f26
timolp <timmothy86264020@gmail.com>
2023-05-08 17:03:49 +0100

    [Refactor + Feat] streamlit spaces // Bold sliders titles // Feature: improved the reading of error codes on the webapp

--- src/stream_lit.py
+++ src/stream_lit.py
@@ -3,15 +3,24 @@ import json
 import requests
 
 st.title("Kidney Stones Machine Learning Algorithm 🗿")
+st.write("#")
 
 st.write("Select the patient's urine details with the sliders below:")
-gravity = st.slider("Gravity (Density)", 1.008, 1.025, 1.008, 0.001, format="%.3f")
-ph = st.slider("pH", 5., 7., 5., 0.01)
-osmo = st.slider("Osmo (mOsm)", 250, 1000, 250)
-cond = st.slider("Conductivity (mMoh)", 9., 35., 9., 0.1, format="%.1f")
-urea = st.slider("Urea (mmol/L)", 75, 600, 75)
-calc = st.slider("Calcium (mmol/L)", 0.2, 12., 0.2, 0.01)
+st.write("#")
 
+# Min and max sliders values are chosen to the naked eye with the training dataset
+gravity = st.slider("**Gravity (Density of the urine)**", 1.008, 1.025, 1.008, 0.001, format="%.3f")
+st.write()
+ph = st.slider("**pH**", 5., 7., 5., 0.01)
+st.write()
+osmo = st.slider("**Osmolarity (mOsm)**", 250, 1000, 250)
+st.write()
+cond = st.slider("**Conductivity (mMoh)**", 9., 35., 9., 0.1, format="%.1f")
+st.write()
+urea = st.slider("**Urea (mmol/L)**", 75, 600, 75)
+st.write()
+calc = st.slider("**Calcium (mmol/L)**", 0.2, 12., 0.2, 0.01)
+st.write("#")
 
 # converting the inputs into a json format
 inputs = {"gravity": gravity,
@@ -22,7 +31,10 @@ inputs = {"gravity": gravity,
           "calc": calc}
 
 # when the user clicks on button it will fetch the API
-if st.button('Click to kiss your cat 👸🏼'):
-    response = requests.post(url="http://127.0.0.1:8000/inference",
-                        data=json.dumps(inputs))
-    st.subheader(response.text[1:-1])
+if st.button('Click to diagnose 👨‍⚕️'):
+    response = requests.post(url="http://127.0.0.1:8000/myapp",
+                             data=json.dumps(inputs))
+    if response.status_code == 200:
+        st.subheader(response.text[1:-1])
+    else:
+        st.subheader(response.text)
